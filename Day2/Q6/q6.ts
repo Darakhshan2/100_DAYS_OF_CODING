bea044d3692137f2f5a4f9eb8c09d88b7cd83631
Darakhshan Juniad <156326631+Darakhshan2@users.noreply.github.com>
2024-05-08 00:05:39 -0700

    Update q6.ts

--- Day2/Q6/q6.ts
+++ Day2/Q6/q6.ts
@@ -1,5 +1,6 @@
 //Q6 - Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
 
 let strippingName =  "\t\n darakhshan Junaid \t\n " ;
-console.log(strippingName.trim());
+console.log(strippingName); //name with whitespaces
+console.log(strippingName.trim()); //name without whitespaces
  
